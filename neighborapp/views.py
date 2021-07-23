1dcbfd5811c7875b203497048727aa916d90280e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-23 22:21:16 +0300

    defined class registration

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def registration(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST) 
+        if form.is_valid():
+           form.save()
+           username = form.cleaned_data.get('username')
+           user_password = form.cleaned_data.get('password1')
+           user = authenticate(username=username, password=user_password)
+           login(request, user)
+        return redirect('login')
+    else:
+        form= SignUpForm()
+    return render(request, 'registration/registration_form.html', {"form":form}) 
