9f37b0ff402d1260ed7621a58d688da18fd0cf9e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-23 21:41:18 +0300

    regiter app in settings

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighborapp.app.NeighborappConfig',
+    'bootstrap4',
+    'cloudinary',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
