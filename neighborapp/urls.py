9c1f959d77d9cbe22f625058082170bd73281918
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-23 22:48:37 +0300

    Add urls for registration

--- neighborapp/urls.py
+++ neighborapp/urls.py
@@ -8,6 +8,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url('register/',views.registration, name='registration'),
+    url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
