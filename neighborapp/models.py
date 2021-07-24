083e41ff747e525c924d294db3e3b42cd9bf895b
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-24 11:53:10 +0300

    Add class model

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import  User
 # Create your models here.
 
 class Neighbourhood(models.Model):
@@ -7,3 +7,31 @@ class Neighbourhood(models.Model):
     neighbourhood_location = models.CharField(max_length=60,blank=False)
     occupants_count = models.IntegerField()
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighbor')
+
+
+
+#class profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    nieghborhood_name = models.CharField(max_length=80, blank=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+#class Bussiness
+class Bussiness(models.Model):
+     business_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+     name = models.CharField(max_length=120)
+     business_email = models.EmailField(max_length=254)
+     neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='business')
+    
+
+   
+
+
+
+
+ 
+
+
