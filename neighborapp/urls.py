ccb239ae5e203e9f14ae3c49a252c63488859bfe
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-24 10:44:08 +0300

    create class neighborhood, make migrations

--- neighborapp/urls.py
+++ neighborapp/urls.py
@@ -9,6 +9,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns=[
     url(r'^$',views.index,name='index'),
     url('register/',views.registration, name='registration'),
+     url('profile/', views.index, name='profile'),
     url('login/',auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
 ]
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -90,11 +90,12 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighborhood',   
+        'USER': 'moringa',      
+        'PASSWORD':'Ammoh',
     }
 }
 
